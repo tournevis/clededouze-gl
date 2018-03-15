db02f568199e4bd9ba49d48c7de7d4b6bb761ad3
Arthur Rob <arthur.ro92@gmail.com>
2018-03-15 14:32:50 +0100

    ⬆️  update dependencies and add webgl setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 'use strict'
-export default class {
+
+class ClededouzeGl {
   constructor () {
-    console.log('Pépito, Hola Muchachos');
+    this.canvas = document.querySelector("#app > canvas")
+    // Initialisation du contexte WebGL
+    this.gl = this.canvas.getContext("webgl")
+
+    // Continuer seulement si WebGL est disponible et fonctionnel
+    if (!this.gl) {
+      alert("Impossible d'initialiser WebGL. Votre navigateur ou votre machine peut ne pas le supporter.")
+      return
+    }
+
+    // Définir la couleur d'effacement comme étant le noir, complètement opaque
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    // Effacer le tampon de couleur avec la couleur d'effacement spécifiée
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT|this.gl.DEPTH_BUFFER_BIT)
   }
 }
